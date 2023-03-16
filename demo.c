9b2ed8d6afa4c298928298a7c3eb9eef015cf2d8
Immortal-Fates <1877114891@qq.com>
2023-03-16 15:46:23 +0800

    test win

--- demo.c
+++ demo.c
@@ -3,6 +3,16 @@ int main(){
     int a;
     printf("Hello\n");
     scanf("%d",&a);
-    printf("%d\n",a);
+    printf("%d\n",a); printf("Hello\n");
+    scanf("%d", &a);
+    printf("%d\n", a); printf("Hello\n");
+    scanf("%d", &a);
+    printf("%d\n", a); printf("Hello\n");
+    scanf("%d", &a);
+    printf("%d\n", a); printf("Hello\n");
+    scanf("%d", &a);
+    printf("%d\n", a); printf("Hello\n");
+    scanf("%d", &a);
+    printf("%d\n", a);
     return 0;
 }
